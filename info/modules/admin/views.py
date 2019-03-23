33106a6e360cbb8fc40b4185f55601652c5befd7
baoyan <baoyan@email.com>
2019-03-23 20:46:46 +0800

    修改表头

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -24,15 +24,15 @@ from . import admin_blue
 @user_login_data
 def admin_login():
     if request.method == "GET":
-        # 去 session 中取指定的值
+
         user_id = session.get("user_id", None)
         is_admin = session.get("is_admin", False)
-        # 如果用户id存在，并且是管理员，那么直接跳转管理后台主页
+
         if user_id and is_admin:
             return redirect(url_for('admin.admin_index'))
         return render_template('admin/login.html')
 
-    # 取到登录的参数
+
     username = request.form.get("username")
     password = request.form.get("password")
     if not all([username, password]):
@@ -69,14 +69,14 @@ def admin_index():
 
 @admin_blue.route('/user_count')
 def user_count():
-    # 查询总人数
+
     total_count = 0
     try:
         total_count = User.query.filter(User.is_admin == False).count()
     except Exception as e:
         current_app.logger.error(e)
 
-    # 查询月新增数
+
     mon_count = 0
     try:
         now = time.localtime()
@@ -86,7 +86,7 @@ def user_count():
     except Exception as e:
         current_app.logger.error(e)
 
-    # 查询日新增数
+
     day_count = 0
     try:
         day_begin = '%d-%02d-%02d' % (now.tm_year, now.tm_mon, now.tm_mday)
@@ -95,15 +95,14 @@ def user_count():
     except Exception as e:
         current_app.logger.error(e)
 
-    # 查询图表信息
-    # 获取到当天00:00:00时间
+
 
     now_date = datetime.strptime(datetime.now().strftime('%Y-%m-%d'), '%Y-%m-%d')
-    # 定义空数组，保存数据
+
     active_date = []
     active_count = []
 
-    # 依次添加数据，再反转
+
     for i in range(0, 31):
         begin_date = now_date - timedelta(days=i)
         end_date = now_date - timedelta(days=(i - 1))
@@ -127,9 +126,7 @@ def user_count():
 
 @admin_blue.route('/user_list')
 def user_list():
-    """获取用户列表"""
 
-    # 获取参数
     page = request.args.get("page", 1)
     try:
         page = int(page)
@@ -137,12 +134,12 @@ def user_list():
         current_app.logger.error(e)
         page = 1
 
-    # 设置变量默认值
+
     users = []
     current_page = 1
     total_page = 1
 
-    # 查询数据
+
     try:
         paginate = User.query.filter(User.is_admin == False).order_by(User.last_login.desc()).paginate(page, constants.ADMIN_USER_PAGE_MAX_COUNT, False)
         users = paginate.items
@@ -151,7 +148,7 @@ def user_list():
     except Exception as e:
         current_app.logger.error(e)
 
-    # 将模型列表转成字典列表
+
     users_list = []
     for user in users:
         users_list.append(user.to_admin_dict())
@@ -163,7 +160,7 @@ def user_list():
 
 @admin_blue.route('/news_review')
 def news_review():
-    """返回待审核新闻列表"""
+
 
     page = request.args.get("page", 1)
     keywords = request.args.get("keywords", "")
@@ -179,12 +176,12 @@ def news_review():
 
     try:
         filters = [News.status != 0]
-        # 如果有关键词
+
         if keywords:
-            # 添加关键词的检索选项
+
             filters.append(News.title.contains(keywords))
 
-        # 查询
+
         paginate = News.query.filter(*filters) \
             .order_by(News.create_time.desc()) \
             .paginate(page, constants.ADMIN_NEWS_PAGE_MAX_COUNT, False)
@@ -207,15 +204,15 @@ def news_review():
 
 @admin_blue.route('/news_review_detail')
 def news_review_detail():
-    """新闻审核"""
+
 
     if request.method == "GET":
-        # 获取新闻id
+
         news_id = request.args.get("news_id")
         if not news_id:
             return render_template('admin/news_review_detail.html', data={"errmsg": "未查询到此新闻"})
 
-        # 通过id查询新闻
+
         news = None
         try:
             news = News.query.get(news_id)
@@ -225,16 +222,16 @@ def news_review_detail():
         if not news:
             return render_template('admin/news_review_detail.html', data={"errmsg": "未查询到此新闻"})
 
-        # 返回数据
+
         data = {"news": news.to_dict()}
         return render_template('admin/news_review_detail.html', data=data)
-        # 执行审核操作
 
-    # 1.获取参数
+
+
     news_id = request.json.get("news_id")
     action = request.json.get("action")
 
-    # 2.判断参数
+
     if not all([news_id, action]):
         return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
     if action not in ("accept", "reject"):
@@ -242,7 +239,7 @@ def news_review_detail():
 
     news = None
     try:
-        # 3.查询新闻
+
         news = News.query.get(news_id)
     except Exception as e:
         current_app.logger.error(e)
@@ -250,11 +247,11 @@ def news_review_detail():
     if not news:
         return jsonify(errno=RET.NODATA, errmsg="未查询到数据")
 
-    # 4.根据不同的状态设置不同的值
+
     if action == "accept":
         news.status = 0
     else:
-        # 拒绝通过，需要获取原因
+
         reason = request.json.get("reason")
         if not reason:
             return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
@@ -272,7 +269,7 @@ def news_review_detail():
 
 @admin_blue.route('/news_edit')
 def news_edit():
-    """返回新闻列表"""
+
 
     page = request.args.get("page", 1)
     keywords = request.args.get("keywords", "")
@@ -288,12 +285,12 @@ def news_edit():
 
     try:
         filters = []
-        # 如果有关键词
+
         if keywords:
-            # 添加关键词的检索选项
+
             filters.append(News.title.contains(keywords))
 
-        # 查询
+
         paginate = News.query.filter(*filters) \
             .order_by(News.create_time.desc()) \
             .paginate(page, constants.ADMIN_NEWS_PAGE_MAX_COUNT, False)
@@ -316,16 +313,16 @@ def news_edit():
 
 @admin_blue.route('/news_edit_detail', methods=["GET", "POST"])
 def news_edit_detail():
-    """新闻编辑详情"""
+
 
     if request.method == "GET":
-        # 获取参数
+
         news_id = request.args.get("news_id")
 
         if not news_id:
             return render_template('admin/news_edit_detail.html', data={"errmsg": "未查询到此新闻"})
 
-        # 查询新闻
+
         news = None
         try:
             news = News.query.get(news_id)
@@ -335,7 +332,7 @@ def news_edit_detail():
         if not news:
             return render_template('admin/news_edit_detail.html', data={"errmsg": "未查询到此新闻"})
 
-        # 查询分类的数据
+
         categories = Category.query.all()
         categories_li = []
         for category in categories:
@@ -344,13 +341,13 @@ def news_edit_detail():
             if category.id == news.category_id:
                 c_dict["is_selected"] = True
             categories_li.append(c_dict)
-        # 移除`最新`分类
+
         categories_li.pop(0)
 
         data = {"news": news.to_dict(), "categories": categories_li}
         return render_template('admin/news_edit_detail.html', data=data)
 
-    # post请求
+
 
     news_id = request.form.get("news_id")
     title = request.form.get("title")
@@ -358,7 +355,7 @@ def news_edit_detail():
     content = request.form.get("content")
     index_image = request.files.get("index_image")
     category_id = request.form.get("category_id")
-    # 1.1 判断数据是否有值
+
     if not all([title, digest, content, category_id]):
         return jsonify(errno=RET.PARAMERR, errmsg="参数有误")
 
@@ -370,7 +367,7 @@ def news_edit_detail():
     if not news:
         return jsonify(errno=RET.NODATA, errmsg="未查询到新闻数据")
 
-    # 1.2 尝试读取图片
+
     if index_image:
         try:
             index_image = index_image.read()
@@ -378,20 +375,20 @@ def news_edit_detail():
             current_app.logger.error(e)
             return jsonify(errno=RET.PARAMERR, errmsg="参数有误")
 
-        # 2. 将标题图片上传到七牛
+
         try:
             key = storage(index_image)
         except Exception as e:
             current_app.logger.error(e)
             return jsonify(errno=RET.THIRDERR, errmsg="上传图片错误")
         news.index_image_url = constants.QINIU_DOMIN_PREFIX + key
-    # 3. 设置相关数据
+
     news.title = title
     news.digest = digest
     news.content = content
     news.category_id = category_id
 
-    # 4. 保存到数据库
+
     try:
         db.session.commit()
     except Exception as e:
@@ -404,13 +401,13 @@ def news_edit_detail():
 
 @admin_blue.route('/news_category')
 def get_news_category():
-    # 获取所有的分类数据
+
     categories = Category.query.all()
-    # 定义列表保存分类数据
+
     categories_dicts = []
 
     for category in categories:
-        # 获取字典
+
         cate_dict = category.to_dict()
         # 拼接内容
         categories_dicts.append(cate_dict)
